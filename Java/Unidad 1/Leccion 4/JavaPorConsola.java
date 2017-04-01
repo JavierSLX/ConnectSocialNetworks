4a26b205adc14e580d554e0c4828b3b53bc9eedc
Javier Serrano Lule <jserranolule@gmail.com>
2017-03-31 22:39:51 -0600

    Java. Unidad 1. Lección 4

---
+++ Java/Unidad 1/Leccion 4/JavaPorConsola.java
@@ -0,0 +1,7 @@
+class JavaPorConsola
+{
+	static public void main(String[] args)
+	{
+		System.out.println("Bienvenido al Curso de Java!");
+	}
+}
---
+++ Java/Unidad 1/Leccion 4/MiPrimerProgramaJava.java
@@ -0,0 +1,7 @@
+class MiPrimerProgramaJava
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Mi primer programa en Java desde comandos");
+	}
+}
